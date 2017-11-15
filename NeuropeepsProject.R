7f9ec6a275070f8635967f547091f9ad85cec992
nlagrange <nicolebutler619@yahoo.com>
2017-11-15 12:45:07 -0800

    Add files via upload

--- NeuropeepsProject.R
+++ NeuropeepsProject.R
@@ -2,4 +2,20 @@ Neuropees
 
 # step 1 : convert .sav file into csv file.
 
+# Done by pam. See TransformData.R
+# Thanks pam!
+
 # paralles step: learn how to upload and download this script to github
+# Use the clone button, save and then add to your folder of your working directory.
+
+
+
+# Step 2a
+# Do we have the same number of subjects/ measurements that the paper reported? Do the variables look right?
+
+# Step 2b
+# write code to output such as histograms and summaries of teh data as used in the paper.
+
+# Step 2c 
+# figure out how to make an R markdown file
+# Transform script into a R markdown file
