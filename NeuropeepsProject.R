6e310bf7ee40955ff0e3f77350ea4229c6055ca7
Pamela Riviere <pamelariviere@Pamelas-MacBook-Pro.local>
2017-11-15 21:01:48 -0800

    Added code for reading in data from TransformData.R into NeuroPeepesProject.R

--- NeuropeepsProject.R
+++ NeuropeepsProject.R
@@ -2,6 +2,16 @@ Neuropees
 
 # step 1 : convert .sav file into csv file.
 
+library(foreign)
+library(dplyr)
+
+setwd('/Users/pamelariviere/CodeRepo/RCode/PSYCH201a/Project') # set PATH to location of .sav file
+
+color.ctxt <- read.spss('/Users/pamelariviere/CodeRepo/RCode/PSYCH201a/Project/ColorContext_PSYCH201A.sav', to.data.frame = T)
+color.ctxt <- color.ctxt[order(color.ctxt$Subject),] # sort the rows by Subject ID
+
+write.csv(color.ctxt, 'color_data_pr.csv') # write the dataframe as csv
+
 # Done by pam. See TransformData.R
 # Thanks pam!
 
