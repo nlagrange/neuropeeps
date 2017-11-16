c1e8de80b98c822edecb187d187b632a478127aa
nlagrange <nicolebutler619@yahoo.com>
2017-11-15 19:07:11 -0800

    Add files via upload

--- NeuropeepsProject.R
+++ NeuropeepsProject.R
@@ -14,7 +14,27 @@ Neuropees
 # Do we have the same number of subjects/ measurements that the paper reported? Do the variables look right?
 
 # Step 2b
-# write code to output such as histograms and summaries of teh data as used in the paper.
+# write code to output such as histograms and summaries of the data as used in the paper.
+
+color_data_pr %>% filter(color=='red', pic=='neu')  %>%
+  ggplot(aes(x=Pic_valence_MEAN))+ geom_histogram(binwidth = .05) + scale_x_continuous(breaks=seq(0,6,.5))+
+  labs(title = 'Red Neutral Valence Ratings', x= 'Valence Mean', y= 'Count')
+
+color_data_pr %>% filter(color=='red', pic=='test')  %>%
+  ggplot(aes(x=Pic_valence_MEAN))+ geom_histogram(binwidth = .05) + scale_x_continuous(breaks=seq(0,6,.5))+ 
+  labs(title = 'Red Test Valence Ratings', x= 'Valence Mean', y= 'Count')
+
+
+color_data_pr %>% filter(color=='red', pic=='neu')  %>%
+  ggplot(aes(x=Pic_arousal_MEAN))+ geom_histogram(binwidth = .05) + scale_x_continuous(breaks=seq(0,6,.5))+
+  labs(title = 'Red Neutral Arousal Ratings', x= 'Arousal Mean', y='Count')
+
+color_data_pr %>% filter(color=='red', pic=='test')  %>%
+  ggplot(aes(x=Pic_arousal_MEAN))+ geom_histogram(binwidth = .05) + scale_x_continuous(breaks=seq(0,6,.5))+
+  labs(title = 'Red Test Arousal Ratings', x = 'Arousal Mean', y= 'Count')
+
+# Nicole red test relevent histo and red test nutral mean arousal
+  # and another for red 
 
 # Step 2c 
 # figure out how to make an R markdown file
